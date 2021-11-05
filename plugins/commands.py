9d1049daf0fa7d94c0aeb670b0e52fe2809e82f5
mujuaj <89645963+mujuaj@users.noreply.github.com>
2021-11-05 19:06:46 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -35,9 +35,9 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('➕ Add Me To Your Groups ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('𝗝𝗢𝗜𝗡 𝗠𝗔𝗜𝗡 𝗖𝗛𝗔𝗡𝗡𝗘𝗟', url=f'https://t.me/AJmovieLINKS')
             ],[
-            InlineKeyboardButton('🔍 Search', switch_inline_query_current_chat=''),
+            InlineKeyboardButton('🔍╽𝗠𝗼𝗿𝗲 𝗠𝗼𝘃𝗶𝗲𝘀╽', url=f'https://t.me/worldmoviesaj'),
             InlineKeyboardButton('🤖 Updates', url='https://t.me/worldmoviesaj')
             ],[
             InlineKeyboardButton('ℹ️ Help', callback_data='help'),
